ab0175f47ddd6f837e395202400e8d1f618ab6d8
Matthieu H <matthieu@holtz.fr>
2013-07-26 14:09:34 +0200

    Change uid from int to UUID

--- framework/src/message/MessageAuthentification.java
+++ framework/src/message/MessageAuthentification.java
@@ -1,24 +1,25 @@
 package message;
 
+import java.util.UUID;
+
 import visitor.Visitor;
 
 public class MessageAuthentification extends Message {
 
-	private int UId;
+	private UUID UId;
 
 	@Override
 	public void accept(Visitor visitor) {
 
-			visitor.encode(this);
-
+		visitor.encode(this);
 
 	}
 
-	public int getUId() {
+	public UUID getUId() {
 		return UId;
 	}
 
-	public void setUId(int uId) {
+	public void setUId(UUID uId) {
 		UId = uId;
 	}
 
