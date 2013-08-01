7609a790b33fa58264740c3b62518b73bb3f11d7
Imy <Imy@Imy-PC>
2013-08-01 18:36:49 +0200

    test MessageAck

--- framework/test/fr/labkira/visitor/TestXMLEncoderVisitor.java
+++ framework/test/fr/labkira/visitor/TestXMLEncoderVisitor.java
@@ -6,7 +6,9 @@ import java.util.UUID;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
+import fr.labkira.message.MessageACK;
 import fr.labkira.message.MessageAuthentification;
+import fr.labkira.message.MessageACK.MessageType;
 import fr.labkira.visitor.XMLEncodoerVisitor;
 
 
@@ -14,13 +16,29 @@ import fr.labkira.visitor.XMLEncodoerVisitor;
 public class TestXMLEncoderVisitor {
 
 	public static void main(String[] args) throws ParserConfigurationException, TransformerException {
-
+		// MessageAuthentification 
 		MessageAuthentification msg = new MessageAuthentification();
 		msg.setUId(UUID.randomUUID());
+		msg.setPassword("imyLab02");
 
 		XMLEncodoerVisitor encoder = new XMLEncodoerVisitor();
 
 		encoder.encode(msg);
+
+		/*ByteArrayOutputStream s = new ByteArrayOutputStream();
+
+		encoder.writeDocTo(s);
+		
+		String content = s.toString();
+
+		System.out.println(content); */
+		
+		// MessageAck 
+		MessageACK mack = new MessageACK();
+		mack.setDescription(" Everything is alright ");
+		mack.setType(MessageType.Ok);
+		
+		encoder.encode(mack);
 
 		ByteArrayOutputStream s = new ByteArrayOutputStream();
 
@@ -29,6 +47,15 @@ public class TestXMLEncoderVisitor {
 		String content = s.toString();
 
 		System.out.println(content);
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 }
