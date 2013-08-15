042f87944584a84f5a709b9cb0c64b36c74ec2ac
Imy <Imy@Imy-PC>
2013-08-15 22:05:57 +0200

    Message commit

--- framework/src/fr/labkira/message/Message.java
+++ framework/src/fr/labkira/message/Message.java
@@ -1,12 +1,16 @@
 package fr.labkira.message;
 
+
+
+import java.io.IOException;
+
 import fr.labkira.visitor.Visitor;
 
 public abstract class Message {
 
 
 
-	public void accept(Visitor visitor) {
+	public void accept(Visitor visitor) throws IOException {
 
 			visitor.encode(this);
 
