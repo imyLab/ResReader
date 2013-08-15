3bba604e2a7580e7d0b5ec4ce72815bbaa7ca439
Imy <Imy@Imy-PC>
2013-08-15 22:35:08 +0200

    MessageAuthentification modif

--- framework/src/fr/labkira/message/MessageAuthentification.java
+++ framework/src/fr/labkira/message/MessageAuthentification.java
@@ -4,11 +4,17 @@ import java.util.UUID;
 
 import fr.labkira.visitor.Visitor;
 
-
 public class MessageAuthentification extends Message {
 
-	private UUID UId; 
+	private UUID UId;
 	private String password;
+	private String UserName;
+
+	public MessageAuthentification(UUID UId, String password, String UserName) {
+		this.setPassword(password);
+		this.setUId(UId);
+		this.setUserName(UserName);
+	}
 
 	@Override
 	public void accept(Visitor visitor) {
@@ -31,6 +37,14 @@ public class MessageAuthentification extends Message {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getUserName() {
+		return UserName;
+	}
+
+	public void setUserName(String userName) {
+		UserName = userName;
 	}
 
 }
