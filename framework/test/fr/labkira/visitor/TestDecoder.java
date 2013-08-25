d6e5c3d43f3dcc27b0afd175d6030c5c7c8711a9
Imy <Imy@Imy-PC>
2013-08-25 11:50:03 +0200

    Test Decode MessageDownload

--- framework/test/fr/labkira/visitor/TestDecoder.java
+++ framework/test/fr/labkira/visitor/TestDecoder.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -20,6 +21,7 @@ import org.xml.sax.SAXException;
 
 import fr.labkira.message.Message;
 import fr.labkira.message.MessageAuthentification;
+import fr.labkira.message.MessageDownload;
 
 public class TestDecoder {
 	
@@ -64,5 +66,48 @@ public class TestDecoder {
 		assertEquals("fr.labkira.message.MessageAuthentification", attribute.getTextContent());
 		System.out.println(message.getTextContent());
 	}
+	
+	@Test
+	public void test_DecodeMessageDownload() throws TransformerException, ParserConfigurationException, SAXException, IOException {
 
+		MessageDownload md = new MessageDownload();
+
+		File file = new File ("image.jpeg");
+		UUID id = UUID.randomUUID();
+		
+		md.setFile(file);
+		md.setUIdPic(id);
+		
+		XMLEncoderVisitor encoder = new XMLEncoderVisitor();
+		encoder.encode(md);
+
+		ByteArrayOutputStream s = new ByteArrayOutputStream();
+		encoder.writeDocTo(s);
+		String content = s.toString();
+		
+		assertNotNull(s);
+				
+		DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder();
+
+		//create document from string xml :
+		Document doc = docBuilder.parse(new ByteArrayInputStream(content.getBytes()));
+
+		assertNotNull(doc);
+		
+		//get element called by tag name. There is only 1 called message in the document, so we want the 1st and sole from the NodeList
+		Node message = doc.getElementsByTagName("message").item(0);
+		assertNotNull(message);
+		
+		//Accessing attributes from the node <message>. There is only 1 called type.
+		Node attribute = message.getAttributes().item(0);
+		assertNotNull(attribute);
+		
+		//And here we got what we want :
+		
+		assertEquals("fr.labkira.message.MessageDownload", attribute.getTextContent());
+		System.out.println(message.getTextContent());
+	}
+	
+	
 }
